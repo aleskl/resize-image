b563700feb8f5ed71a9891da9b9ad787d4ab3e9b
Aleš Kladnik <ales.kladnik@gmail.com>
2023-08-30 12:13:23 +0200

    Add files via upload

---
+++ resizepad.sh
@@ -0,0 +1,14 @@
+# resize image to fixed size with padding using ImageMagick
+# set desired size below (960x400 is the Grav header image size)
+# https://superuser.com/questions/1358715/how-do-i-resize-and-pad-an-image-to-a-given-size-using-imagemagick
+# https://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
+#
+# example usage:
+# sh resizepad.sh /mnt/c/Users/aleskl/Downloads/image.jpg
+
+size="960x400"
+extension="${1##*.}"
+filename="${1%.*}"
+convert "$1" -resize $size -background white -gravity center -extent $size "${filename}_${size}.${extension}"
+echo "Output:" "${filename}_960x400.${extension}"
+
